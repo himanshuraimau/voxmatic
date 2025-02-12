6a5ceeef2c90d910dd7e84d0ebfcf3ddd66ab426
Himanshu Rai <himanshuraimau9@gmail.com>
2025-02-12 15:25:53 +0530

    Refactor TodoListScreen styles and add initial layout component

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack>
+      {/* You can add screen options here that apply to all screens in the stack */}
+      {/* For example: */}
+      {/* <Stack.Screen name="index" options={{ title: 'Home' }} /> */}
+    </Stack>
+  );
+}
--- app/screens/TodoListScreen.tsx
+++ app/screens/TodoListScreen.tsx
@@ -101,13 +101,7 @@ const styles = StyleSheet.create({
         padding: 15,
         marginBottom: 10,
         shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 1.41,
-        elevation: 2,
+        boxShadow: '0px 1px 3px rgba(0, 0, 0, 0.2)',
     },
     todoContent: {
         flexDirection: 'row',
